01a75a2fd33c01ff9583057b374e7057b4115a85
faustienf <faustienf@protonmail.com>
2024-03-16 23:52:44 +0200

    log duration

--- src/shared/heavy-component.tsx
+++ src/shared/heavy-component.tsx
@@ -2,6 +2,7 @@ export const HeavyComponent = ({ duration }: { duration: number }) => {
   const now = Date.now();
   while (Date.now() - now < duration) {
     // ¯\_(ツ)_/¯
+    console.log('¯_(ツ)_/¯', Date.now() - now);
   }
 
   return null;
